7dc09f7c1647cd2596543a3f25c9c1b32fc17353
Vanika Patel <vanikadpatel@gmail.com>
2022-01-29 16:01:07 +0000

    created header of the homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1> Let's build the google clone!!</h1>
+ <Router>
+        <Switch>
+          <Route path="/search">
+            {/* <SearchPage /> */}
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
